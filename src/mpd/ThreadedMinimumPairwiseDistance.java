936aed98e84736e9a651ad09c3ad60013ee94237
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-26 23:56:46 -0600

    Added some commits after doing some reading

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -4,7 +4,10 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 
     @Override
     public int minimumPairwiseDistance(int[] values) {
+      // Building a thread, maybe 4 differnt to cover the assigment.
+      // need to have Thread, and join in this way
         throw new UnsupportedOperationException();
     }
+
 
 }
