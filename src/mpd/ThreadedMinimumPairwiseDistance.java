bd732117b412d3a8939c261200c41be0ab5ea9c3
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-27 10:42:47 -0600

    Working on the code, thought about making an ambougut that would handle the ith process but just going to make 4 difffernt methods to handle the four differnt sections that the lab calls for

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -4,10 +4,25 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 
     @Override
     public int minimumPairwiseDistance(int[] values) {
-      // Building a thread, maybe 4 differnt to cover the assigment.
-      // need to have Thread, and join in this way
-        throw new UnsupportedOperationException();
+      Thread lowerleft = new Tread(new lowerleft(values, min));
+      Thread bottomright = new Thread(new bottomright(values, min));
+      Thread topright = new Thread(new topright(values, min));
+      Thread cetner = new Thread(new center(vales, min));
+      // Initalizing the threads.
+      lowerleft.start();
+      bottomright.start();
+      topright.start();
+      cetner.start();
+      //Joining all of the threads together needed since we are multithreading
+      try{
+        lowerleft.join();
+        bottomright.join();
+        topright.join();
+        center.join();
+      }
+      catch(InterruptedException noGo){
+        noGO.printStactTrace();
+      }
+      return min.getMin();
     }
-
-
-}
+//Need to add the acutall code to handle of different thread called, proprly similar to that of peters code in the workup
